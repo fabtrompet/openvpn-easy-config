27af11c7674aff1255ee42f5388af66ee7cc74a4
fabtrompet <correiafabio063@gmail.com>
2020-12-03 09:58:16 -0300

    Update mkvpnclient.sh

--- mkvpnclient.sh
+++ mkvpnclient.sh
@@ -1,6 +1,5 @@
-cd /tmp/vpn/openvpn-certs/server/easyrsa3
-./easyrsa build-client-full <vpnclient>
-cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/issued/<vpnclient>.crt /etc/openvpn/
-cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/private/<vpnclient>.key /etc/openvpn/
+cd /etc/openvpn/openvpn-certs/server/easyrsa3
+./easyrsa build-client-full $1
 cd /etc/openvpn/
-./mkinline.sh -i client.conf -o <client_name>.ovpn
+./makeconf.sh $1 > $1.conf
+./mkinline.sh -i $1.conf -o $1.ovpn
