fa1d4a860856d505fff9cd44deb96313872e8c7e
fabtrompet <correiafabio063@gmail.com>
2020-12-03 09:54:16 -0300

    Create makeconf.sh

---
+++ makeconf.sh
@@ -0,0 +1,17 @@
+echo "client
+dev tun
+;proto udp
+proto tcp
+remote <ip> <port>
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+ca /etc/openvpn/ca.crt
+cert /etc/openvpn/$1.crt
+key /etc/openvpn/$1.key
+remote-cert-tls server
+tls-auth /etc/openvpn/ta.key 1
+comp-lzo
+verb 3
+"
