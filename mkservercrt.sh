b288b06820b716cec4264f0adf62e2864e2d10b7
fabtrompet <correiafabio063@gmail.com>
2020-12-03 09:51:51 -0300

    Update mkservercrt.sh

--- mkservercrt.sh
+++ mkservercrt.sh
@@ -2,8 +2,8 @@ cd /tmp
 wget -c https://github.com/OpenVPN/easy-rsa/archive/master.zip
 mv master.zip easy-rsa.zip
 unzip easy-rsa.zip
-cd /tmp/vpn && mkdir -p openvpn-certs/server
-cd /tmp/vpn/openvpn-certs/server
+cd /etc/openvpn && mkdir -p openvpn-certs/server
+cd /etc/openvpn/openvpn-certs/server
 cp -ar /tmp/easy-rsa-master/* .
 cd easyrsa3
 cp vars.example vars
